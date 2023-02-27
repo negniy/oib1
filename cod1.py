b427bb0fc300ae92707c393cd427da23c83ffd95
Dmitrii Stepanov <dstepanov200498@gmail.com>
2023-02-27 13:35:23 +0400

    1 часть лабораторной выполнена. Шифрование Цезарем

--- cod1.py
+++ cod1.py
@@ -1,5 +1,19 @@
 def main():
     print("start")
+    alfavit="АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    sdvig=int(input('Ключ шифрования: '))%33
+    text=input("Шифруемый текст: ").upper()
+    after_text=''
+    for i in text:
+        mesto = alfavit.find(i)
+        new_mesto = mesto + sdvig
+        if i in alfavit:
+            after_text += alfavit[new_mesto]
+        else:
+            after_text += i
+    print()
+    print(after_text)
+    print("end0")
     
 if __name__ == "__main__":
     main()
